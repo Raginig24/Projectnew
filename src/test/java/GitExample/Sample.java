ee44065913153da89ae2e4c30f626a88f69be4b0
Raginig24 <raginig95@gmail.com>
2021-02-25 12:37:53 +0530

    new project

---
+++ src/test/java/GitExample/Sample.java
@@ -0,0 +1,12 @@
+package GitExample;
+
+
+public class Sample {
+
+public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\vinothinig\\eclipse-workspace\\Selenium\\Drivers\\chromedriver.exe");
+
+		System.out.println("Test1");
+}
+}
